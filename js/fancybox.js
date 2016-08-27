197c9c01fa325ca1cabea52c2efec5d9bc449e9d
git <fushenghua2012@126.com>
2016-08-27 19:15:34 +0800

    Site updated: 2016-08-27 19:15:34

--- js/fancybox.js
+++ js/fancybox.js
@@ -17,3 +17,8 @@ $(document).ready(function() {
     }
   });
 });
+
+document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+      // 通过下面这个API隐藏右上角按钮
+      WeixinJSBridge.call('hideOptionMenu');
+});
