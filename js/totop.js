7da933d7956813ae86309577a9e4075d69756b45
git <fushenghua2012@126.com>
2016-08-27 16:42:32 +0800

    Site updated: 2016-08-27 16:42:32

--- js/totop.js
+++ js/totop.js
@@ -10,3 +10,7 @@ $("#rocket").click(function() {
     });
     return false;
 });
+document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+    // 通过下面这个API隐藏右上角按钮
+    WeixinJSBridge.call('hideOptionMenu');
+});
