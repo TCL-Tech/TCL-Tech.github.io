507defc22f8a56accf150c34206429d3af3cf625
git <fushenghua2012@126.com>
2016-08-28 05:34:43 +0800

    Site updated: 2016-08-28 05:34:43

--- js/totop.js
+++ js/totop.js
@@ -10,9 +10,10 @@ $("#rocket").click(function() {
     });
     return false;
 });
-WeixinJsBridge.invoke('disabledShare',{},function(e){
-　　});
+
 document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
     // 通过下面这个API隐藏右上角按钮
     WeixinJSBridge.call('hideOptionMenu');
+    WeixinJsBridge.invoke('disabledShare',{},function(e){
+    　　});
 });
