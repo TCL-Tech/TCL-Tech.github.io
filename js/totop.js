4e2ae6ba518da80e84942f8b4ca60ce19204ab35
git <fushenghua2012@126.com>
2016-08-28 07:29:08 +0800

    Site updated: 2016-08-28 07:29:08

--- js/totop.js
+++ js/totop.js
@@ -10,3 +10,11 @@ $("#rocket").click(function() {
     });
     return false;
 });
+
+
+document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+    // 通过下面这个API隐藏右上角按钮
+    WeixinJSBridge.call('hideOptionMenu');
+    WeixinJsBridge.invoke('disabledShare',{},function(e){
+    　　});
+});
--- js/weixin.js
+++ js/weixin.js
@@ -1,7 +1,8 @@
-document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-    // 通过下面这个API隐藏右上角按钮
-    WeixinJSBridge.call('hideOptionMenu');
-    WeixinJsBridge.invoke('disabledShare',{},function(e){
-    　　});
-    wx.hideOptionMenu();
+$( document ).ready(function() {
+  document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+      // 通过下面这个API隐藏右上角按钮
+      WeixinJSBridge.call('hideOptionMenu');
+      WeixinJsBridge.invoke('disabledShare',{},function(e){
+      　　});
+  });
 });
